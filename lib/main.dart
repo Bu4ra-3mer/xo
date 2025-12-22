b60a4f30e4844622e6b9a4341ca7120e75bbd9b7
Bu4ra-3mer <boshraamer30@gmail.com>
2025-12-22 23:15:57 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: XOHome());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: XOHome());
   }
 }
 
